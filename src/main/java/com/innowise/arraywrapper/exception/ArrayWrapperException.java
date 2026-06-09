4bcaeb147e6dd11a5e12f02c9de11bbff1a8fea3
alexp <alexpozhigan01@gmail.com>
2026-06-09 17:19:48 +0300

    Inherit Exception rather than RuntimeException.

--- src/main/java/com/innowise/arraywrapper/exception/ArrayWrapperException.java
+++ src/main/java/com/innowise/arraywrapper/exception/ArrayWrapperException.java
@@ -4,7 +4,7 @@ package com.innowise.arraywrapper.exception;
 /**
  * Custom exception for array wrapper operations.
  */
-public class ArrayWrapperException extends RuntimeException {
+public class ArrayWrapperException extends Exception {
 
     public ArrayWrapperException(String message) {
         super(message);
