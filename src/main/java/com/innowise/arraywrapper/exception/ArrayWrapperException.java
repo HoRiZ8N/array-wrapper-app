e657041133e2ee8ad0053475f9e6fe94eb758a09
alexp <alexpozhigan01@gmail.com>
2026-06-16 19:27:38 +0300

    Add standard constructors to ArrayWrapperException

--- src/main/java/com/innowise/arraywrapper/exception/ArrayWrapperException.java
+++ src/main/java/com/innowise/arraywrapper/exception/ArrayWrapperException.java
@@ -1,12 +1,23 @@
 package com.innowise.arraywrapper.exception;
 
-
 /**
  * Custom exception for array wrapper operations.
  */
 public class ArrayWrapperException extends Exception {
 
+    public ArrayWrapperException() {
+        super();
+    }
+
     public ArrayWrapperException(String message) {
         super(message);
+    }
+
+    public ArrayWrapperException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ArrayWrapperException(Throwable cause) {
+        super(cause);
     }
 }
